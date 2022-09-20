95886af3e680208e3ecc050c233fa56239369285
M1kun <m2micic@uwaterloo.ca>
2022-09-19 22:10:22 -0400

    update (visual, socials)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -55,7 +55,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/mihajlo-micic/"><img src={navIcon1} alt="" /></a>
                 {/* <a href="#"><img src={navIcon2} alt="" /></a> */}
                 {/* <a href="#"><img src={navIcon3} alt="" /></a> */}
               </div>
